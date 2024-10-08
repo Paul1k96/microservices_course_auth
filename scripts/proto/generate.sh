606329a650a29fc8da2dff378232ccf9128fbe7c
Timofeev Pavel <pavel.timofeev.art@gmail.com>
2024-10-08 18:39:23 +0300

    feat(user): add user grpc implementation

---
+++ scripts/proto/generate.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# Get current directory
+DIR="$(pwd)"
+
+# Find all directories containing at least one protofile.
+for dir in $(find "${DIR}/api/proto" -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq); do
+    files=$(find "${dir}" -name '*.proto')
+
+    mkdir -p "${DIR}/pkg/proto/gen/$(dirname $(dirname $dir))"
+
+    protoc -I="${DIR}/api/proto" \
+        --go_out=paths=source_relative:"${DIR}/pkg/proto/gen" \
+        --plugin=protoc-gen-go=bin/protoc-gen-go \
+        --go-grpc_out=paths=source_relative:"${DIR}/pkg/proto/gen" \
+        --plugin=protoc-gen-go-grpc=bin/protoc-gen-go-grpc \
+        ${files}
+done
